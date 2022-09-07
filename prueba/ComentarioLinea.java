e64fbef22e0f026057107764c2c5154131dd09db
KilianMartin <85364675+ItsSorey@users.noreply.github.com>
2022-09-07 14:02:00 +0200

    Add files via upload

---
+++ prueba/ComentarioLinea.java
@@ -0,0 +1,9 @@
+package prueba;
+
+public class ComentarioLinea {
+	public static void main(String args[]) {
+		//System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DEL PROGRAMA");
+	}
+}
